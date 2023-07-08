928ee21d31fbc7d38b08aa3594f2fc094d22fb91
Sandeep <sandy6862g22@gmail.com>
2023-07-08 19:10:17 +0530

    added css to index page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,18 @@
+import Image from "next/image"
+import styles from "./page.module.css"
+import Hero from "public/hero.png"
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your products.</h1>
+        <p className={styles.desc}>Turning your idea into Reality. We bring together the teams from the global tech industry</p>
+        <button className={styles.btn}>See Our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="hero image" className={styles.img} />
+      </div>
     </div>
   )
 }
