abef3ad51c6c1f595db5ac01d22def02cb0b89f9
Sandeep <sandy6862g22@gmail.com>
2023-07-12 16:22:49 +0530

    write the portfolio category page

--- src/app/portfolio/[category]/page.js
+++ src/app/portfolio/[category]/page.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from "./page.module.css"
+import { Button } from '@/components'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({params}) => {
   return (
-    <div>Category</div>
+    <div className={styles.container} >
+      <h1 className={styles.catTitle} >{params.category}</h1>
+      {}
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title} >test</h1>
+          <p className={styles.desc} >desc</p>
+          <Button url="#" text="See more" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" alt="img" fill className={styles.img} />
+        </div>
+      </div>
+    </div>
   )
 }
 
