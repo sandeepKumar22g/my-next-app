a8f97b16485a10510f25cfe811122bc24ee480b0
Sandeep <sandy6862g22@gmail.com>
2023-07-18 12:31:28 +0530

    added authentication

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialProvier from "next-auth/providers/credentials"
+import connect from "@/utils/db"
+import User from "@/models/User"
+import bcrypt from "bcryptjs"
 
 const handler = NextAuth({
   providers: [
@@ -7,7 +11,37 @@ const handler = NextAuth({
       clientId: process.env.NEXT_APP_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_APP_GOOGLE_CLIENT_SECRET,
     }),
+    CredentialProvier({
+      id: "credentials",
+      name: "credentials",
+      async authorize(credentials){
+        await connect()
+        try {
+          console.log(credentials)
+          const user = await User.findOne({email: credentials.email})
+          console.log(user)
+          if (user !== null){
+            console.log("it is he", user)
+            
+            const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+            if(isPasswordCorrect){
+              return user
+            }else{
+              throw new Error("Wrong email or password")
+            }
+
+         
+        }
+
+        } catch (err) {
+          throw new Error(err)
+        }
+      }
+    })
   ],
+  pages:{
+    error: "/dashboard/login"
+  }
 })
 
 export {handler as GET, handler as POST}
